6e4dd5710fe814e8d479613fc4aafecca04a2ef8
pramod <pramodmaraganti9@gmail.com>
2024-08-06 07:59:37 +0530

    pushing roshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -29,21 +29,56 @@ N="\e[0m"
                 echo "you are root  user"
 fi
 
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "disable nodejs catalogue "
+ 
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "enable nodejs:18 catalogue "
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "dnf install catalogue "
+
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then 
+useradd roboshop 
+VALIDATE $? "roboshop user creation  "
+else 
+echo -e " Roboshop user already exit $Y skipping $N "
+fi
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "/app  "
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "/tmp/catalogue.zip   "
+
+cd /app  &>> $LOGFILE
+VALIDATE $? "/tmp/catalogue.zip   "
+
+unzip -0 /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "/tmp/catalogue.zip  unzip "
+
+npm install  &>> $LOGFILE
+VALIDATE $? "installing the npm "
+
+cp   /home/centos/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "configuration catalogue "
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "daemon-reload catalogue "
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "enable catalogue catalogue "
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "start catalogue catalogue "
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "mongo.repon catalogue "
 
-VALIDATE $? "cpoied monogDB repo"
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "install mongodb-org-shell catalogue "
 
-dnf install mongodb-org -y &>> $LOGFILE
-VALIDATE $? "DNF monogDB "
-
-systemctl enable mongod &>> $LOGFILE
-VALIDATE $? "ENABLE monogDB "
-
-systemctl start mongod &>> $LOGFILE
-VALIDATE $? "START monogDB "
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
-VALIDATE $? "Remote access monogDB "
-
-systemctl restart mongod &>> $LOGFILE
-VALIDATE $? "systemctl restring the monogDB "
+mongo --host 54.174.138.230 </app/schema/catalogue.js
+VALIDATE $? "loading catalogue data to mogodb "
